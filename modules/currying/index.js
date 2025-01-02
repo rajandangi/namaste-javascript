1ab194e4da578ad6e2701ec88c8a66194ff508b4
rajandangi <rajandangi649@gmail.com>
2025-01-02 22:37:20 +1030

    ADD: implement currying functions in index.js to demonstrate functional programming techniques

---
+++ modules/currying/index.js
@@ -0,0 +1,30 @@
+let multiply = function (x, y) {
+    console.log(x * y);
+};
+
+let multiplyByTwo = multiply.bind(this, 2);
+multiplyByTwo(5); // 10
+
+let multiplyByThree = multiply.bind(this, 3);
+multiplyByThree(5); // 15
+
+let multiplyByFour = multiply.bind(this);
+multiplyByFour(4, 5); // 20
+
+/**
+ * Currying is a functional programming technique where
+ * - a function with multiple arguments is transformed into a series of functions, each taking a single argument
+ * - Instead of taking all arguments at once, the curried function takes the first argument, returns a new function
+ * that takes the next argument, and so on until all arguments are provided. The final function then returns the result.
+ *
+ * In the above example, the multiply function is curried by binding the first argument to a fixed value.
+ * This creates a new function that takes the second argument and returns the result.
+ */
+
+let sum = function (a) {
+    return function (b) {
+        console.log(a + b);
+    };
+}
+let addTwo = sum(2);
+addTwo(3); // 5
